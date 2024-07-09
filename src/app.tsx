69b2a0ca5aed4ddd4f4cc0cd7bd25c2b6165719e
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-07-09 12:43:09 +0100

    05

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,13 @@
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl px-6 text-center">
+        <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua proxíma viagem!</p>
+      
+        <p>Ao planejar sua viagem pela planner você automaticamente concorda com nossos
+        <a href="#">Termos de uso</a> e <a href="#">Política de privacidade</a>.
+        </p>
+      </div>
+    </div>
   )
 }
