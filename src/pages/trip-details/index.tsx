672b79626eba4cf89b4b483f43c04f37f6ec7164
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-07-11 11:29:42 +0100

    01

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,3 +1,44 @@
+import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
+
 export function TripDetailsPage() {
-  return <h1>Trip details!</h1>
+  return (
+    <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
+      <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <MapPin className="size-5 text-zinc-400"/>
+          <span className=" text-zinc-100">Florianópolis, Brasil</span>
+        </div>
+
+        <div className="flex items-center gap-5">
+          <div className="flex items-center gap-2">
+            <Calendar className="size-5 text-zinc-400"/>
+            <span className=" text-zinc-100">17 a 23 de Agosto</span>
+          </div>
+
+          <div className="w-px h-6 bg-zinc-800" />
+
+          <button  className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+            Alterar local/data
+            <Settings2 className="size-5" />
+          </button>
+        </div>
+      </div>
+
+      <main className="flex gap-16">
+        <div className="flex-1">
+          <div className="flex items-center justify-between">
+            <h2 className="text-3xl font-semibold">Atividades</h2>
+            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+              Continuar
+              <ArrowRight className="size-5" />
+            </button>
+          </div>
+        </div>
+
+        <div className="w-80">
+
+        </div>
+      </main>
+    </div>
+  )
 }
